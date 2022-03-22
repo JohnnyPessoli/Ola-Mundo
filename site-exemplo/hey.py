54fa513982db4d8dec4434c2ee47d9b37cfcc2b9
Johnny Pessoli <pessoly@gmail.com>
2022-03-22 05:44:22 -0300

    Create hey.py

---
+++ site-exemplo/hey.py
@@ -0,0 +1,10 @@
+# First
+# Hello World
+
+import developer skill
+import resilience skill
+import persistence skill
+
+pythonapprentice = str('Johnny')
+print(f'welcome to the python world {pythonapprentice}')
+print('Learning...')
